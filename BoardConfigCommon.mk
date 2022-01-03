9dd655c49b9a93c96eabc6c0841505949c9646fd
Orel <Orely4242@gmail.com>
2022-01-03 10:56:42 +0200

    samsung: adapt for Evolution X

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,7 +128,7 @@ VENDOR_SECURITY_PATCH := 2021-10-01
 
 ## SELinux
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
-include device/lineage/sepolicy/exynos/sepolicy.mk
+include device/evolution/sepolicy/exynos/sepolicy.mk
 include device/samsung_slsi/sepolicy/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
